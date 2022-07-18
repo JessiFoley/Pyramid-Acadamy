ce6c27affbc375958f7030a9397e97ff07039f7d
Jess <tfreyberger@gmail.com>
2022-07-18 12:14:40 -0500

    added option 2 to dragon cave

--- Dragon Cave/src/DragonCave.java
+++ Dragon Cave/src/DragonCave.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class DragonCave {
     public static void main(String[] args) {
         Scanner getInput = new Scanner(System.in);
+        String input = "";
 
         System.out.println("You are in a land full of dragons. In front of you, ");
         System.out.println("you see two caves. In one cave, the dragon is friendly");
@@ -11,15 +12,22 @@ public class DragonCave {
         System.out.println("Which cave will you go into?(1 or 2)");
 
         try {
-			String input = getInput.nextLine();
+			input = getInput.nextLine();
         } catch (Exception e) {
         	System.out.println(e.getMessage());
         }
 
-        System.out.println("You approach the cave...");
-        System.out.println("It is dark and spooky...");
-        System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
-        System.out.println("Gobbles you down in one bite!");
+        if (input.equals("1")) {
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+            System.out.println("Gobbles you down in one bite!");
+        } else {
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+            System.out.println("He drops a pile of money on the ground for you!");
+        }
         
         getInput.close();
     }
